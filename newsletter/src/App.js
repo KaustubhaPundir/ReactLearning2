b159fd87b3109db2762bf75afd6406a9be64bb32
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-12 11:37:50 +0530

    added navbar and some news component

--- newsletter/src/App.js
+++ newsletter/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar'
 import React, { Component } from 'react'
-
+import News from './components/News'
 export default class App extends Component {
   name="Kaustubha";
   render() {
     return (
       <div>
-        <h1>hello my name is {this.name} & this is my first class based Component</h1>      
+        <Navbar/>
+        <News/>
       </div>
     )
   }
---
+++ newsletter/src/components/News.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+        This is a news Component
+        <NewsItem/>
+      </div>
+    )
+  }
+}

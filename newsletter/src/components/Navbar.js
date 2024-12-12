47307f4bc99a21adabd4b9f5c024477ddea22af9
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-12 12:04:14 +0530

    got api key and added cards for newsitem

--- newsletter/src/components/Navbar.js
+++ newsletter/src/components/Navbar.js
@@ -9,19 +9,19 @@ export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        {/* <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg> */}
-        <span class="fs-4">Simple header</span>
+        <div className="container">
+    <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        {/* <svg className="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg> */}
+        <span className="fs-4">Simple header</span>
       </a>
 
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      <ul className="nav nav-pills">
+        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
       </ul>
     </header>
   </div>
--- newsletter/src/components/News.js
+++ newsletter/src/components/News.js
@@ -3,16 +3,31 @@ import PropTypes from 'prop-types'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
+    static propTypes = {
+        prop: PropTypes
+    }
+    //   https://newsapi.org/v2/top-headlines?country=in&apiKey=API_KEY
 
-  render() {
-    return (
-      <div>
-        This is a news Component
-        <NewsItem/>
-      </div>
-    )
-  }
+    //   73b8ea3bc8464e5389b4156d048c2aee
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <h2>NewsMonkey-Top Headlines</h2> 
+
+                    <div className="row">
+                        <div className="col-md-4">
+                            <NewsItem title="mytitle" description="mydesc" />
+                        </div>
+                        <div className="col-4">
+                            <NewsItem title="mytitle" description="mydesc" />
+                        </div>
+                        <div className="col-4">
+                            <NewsItem title="mytitle" description="mydesc" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
